f2667d8802e33bc5fb771ce5f1d12f960c96c6e1
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-09-26 23:43:56 +0530

    Added controllers and routes !

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3500;
 
+app.use('/', require('./routes'))
+
 app.listen(port, function(err){
         if(err){
             console.log(`Error occured on server, ${err}`);
