b2f2873d29b7f98c737af1da8d6b8cd9f4272b69
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-09-27 00:56:09 +0530

    Rendering Views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res){
-    res.end( '<h1>Hello world</h1>' )
+    
+    res.render('home',{
+        title: "Shekhar"
+    });
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(req, res){
-    res.send('<h1>Hey this a userController</h1>')
+    res.render('users', {
+        title: 'Guddu'
+    })
 }
